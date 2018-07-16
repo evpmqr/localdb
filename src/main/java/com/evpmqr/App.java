86b8056ebaf9adbe37cfc5b230622873b9f1e444
evpmqr <vuexx427@umn.edu>
2018-07-16 10:57:50 -0500

    Added log4j removed all logging for now

--- src/main/java/com/evpmqr/App.java
+++ src/main/java/com/evpmqr/App.java
@@ -11,12 +11,18 @@ public class App {
         Session session = HibernateUtil.getSessionFactory().openSession();
 
         session.beginTransaction();
-        Employee person = new Employee("Eric", "Vue", 1);
+
+        Employee person = new Employee("Rew", "Vue", 34);
 
         session.save(person);
         session.getTransaction().commit();
+
+
         List<?> results = session.createQuery("FROM Employee as E").list();
+
+
         results.forEach(l -> System.out.println(l.toString()));
-        HibernateUtil.shutdown();
+        Employee employee = session.find(Employee.class, 1);
+        System.out.println(employee);
     }
 }
